e3cb300fead6a0a56cdddc2f9d6279fd0576aa4b
Fernando De Mennato <fdemennato@progesoftware.it>
2020-06-06 10:23:01 +0200

    Change image

--- GitFlow.VS.Extension/GitFlow.VS.ExtensionPackage.cs
+++ GitFlow.VS.Extension/GitFlow.VS.ExtensionPackage.cs
@@ -1,11 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.Shell;
-using TeamExplorer.Common;
 using System;
+using System.Runtime.InteropServices;
 using System.Threading;
-using EnvDTE80;
-using EnvDTE;
+using TeamExplorer.Common;
 
 namespace GitFlowVS.Extension
 {
@@ -15,7 +13,6 @@ namespace GitFlowVS.Extension
     [Guid(GuidList.GuidGitFlowVsExtensionPkgString)]
     public sealed class GitFlowVSExtension : AsyncPackage
     {
-        private DTEEvents _dteEvents;
 
         public GitFlowVSExtension()
         {
@@ -24,20 +21,8 @@ namespace GitFlowVS.Extension
         protected override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             UserSettings.ServiceProvider = this;
-            base.Initialize();
-
-            var dte = (DTE2)GetService(typeof(SDTE));
-            _dteEvents = dte.Events.DTEEvents;
-            _dteEvents.OnStartupComplete += OnStartupComplete;
             return System.Threading.Tasks.Task.FromResult<object>(null);
         }
 
-
-        private void OnStartupComplete()
-        {
-            _dteEvents.OnStartupComplete -= OnStartupComplete;
-            _dteEvents = null;
-            GitFlowScriptInstallation.Install();
-        }
     }
 }
