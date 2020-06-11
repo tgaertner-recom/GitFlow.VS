2e41985633a8835ececc8ff1e760e0d372e976ea
Fernando De Mennato <fdemennato@progesoftware.it>
2020-06-11 21:20:56 +0200

    Fix start vsix

--- GitFlowWithPR.VS.Extension/GitFlowNavigationItem.cs
+++ GitFlowWithPR.VS.Extension/GitFlowNavigationItem.cs
@@ -9,7 +9,7 @@ using TeamExplorer.Common;
 
 namespace GitFlowWithPRVS.Extension
 {
-    [TeamExplorerNavigationItem("80E09B6E-FD3E-4E9B-9894-A9DE8671F870", 1500, TargetPageId = "75EC888F-7549-4D9C-9967-4869A9AEDDDD")]
+    [TeamExplorerNavigationItem("59168500-14BC-4EE9-BB1F-7B2B970A4AF6", 1500, TargetPageId = "75EC888F-7549-4D9C-9967-4869A9AEDDDD")]
     public class GitFlowNavigationItem : TeamExplorerBaseNavigationItem
     {
         private readonly ITeamExplorer teamExplorer;
